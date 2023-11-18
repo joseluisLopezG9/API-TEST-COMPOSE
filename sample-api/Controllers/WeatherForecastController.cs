5279a9cc6f36c0c1265d66fd1a41cae1eb40ca9c
joseluisLopezG9 <duster300g@gmail.com>
2023-11-17 22:11:29 -0600

    Upload project

---
+++ sample-api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace sample_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+
+    [HttpPost(Name = "PostWeatherForecast")]
+    public IActionResult Post([FromBody] WeatherForecast forecastRequest)
+    {
+        if (forecastRequest == null) return BadRequest();
+
+        if (forecastRequest.TemperatureC < -20) return BadRequest("TemperatureC can't be less than -20");
+
+        return Ok();
+    }
+}
